83a2471f69f905ef89b9bdfa346ac1d39a5f73d8
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-02-21 19:52:17 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,16 @@ set -eu
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-cp -irv dotfiles/. ~
+dotfiles=$(ls -A dotfiles)
+
+for f in $dotfiles; do
+  read -r -p "Copy dotfile $f to home? (Y/n) " response
+  case $response in
+    [nN][oO]|[nN])
+      echo "Skipping $f"
+      ;;
+    *)
+      cp -r "dotfiles/$f" ~
+      ;;
+  esac
+done
