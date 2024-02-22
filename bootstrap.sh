a0e3e6812b778f902944b04aa6b872de4c0cc1ed
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-02-22 06:59:06 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,19 @@
 
 set -eu
 
+force_copy=false
+while getopts "f" opt; do
+  case $opt in
+    f)
+      force_copy=true
+      ;;
+    *)
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND - 1))
+
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 dotfiles=${1:-""}
@@ -10,15 +23,17 @@ if [[ -z "$dotfiles" ]]; then
 fi
 echo -e "Dotfiles:\n$dotfiles\n"
 
-read -r -p "Copy dotfiles to home? (y/N) " response
-case $response in
-  [yY][eE][sS]|[yY])
-    ;;
-  *)
-    echo "Exiting"
-    exit 0
-    ;;
-esac
+if [[ "$force_copy" == false ]]; then
+  read -r -p "Copy dotfiles to home? (y/N) " response
+  case $response in
+    [yY][eE][sS]|[yY])
+      ;;
+    *)
+      echo "Exiting"
+      exit 0
+      ;;
+  esac
+fi
 
 for file in $dotfiles; do
   echo "Copying $file to home"
