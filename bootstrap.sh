85fe67e1aeb3995668a46b0f4c9bc258d76f2a26
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-03-28 11:01:47 +0100

    Use stow to create symlinks

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")/dotfiles" || exit
-rsync -avh --no-perms . ~
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
+
+# Build config
+declare -A config_dir
+config_dir["bash"]="$HOME"
+config_dir["git"]="$HOME"
+config_dir["pipewire"]="$HOME/.config/pipewire"
+# config_dir["quodlibet"]="$HOME/.config/quodlibet"
+
+# Create symbolic links
+for package in "${!config_dir[@]}"; do
+  target_dir="${config_dir[$package]}"
+  stow -v -R -t "$target_dir" "$package"
+done
+
 source ~/.bash_profile
